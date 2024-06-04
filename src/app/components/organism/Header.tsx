281dff231a07d9065ffa2f9cd789a6725dfba06e
JSBzz <101786987+JSBzz@users.noreply.github.com>
2024-06-04 15:29:48 +0900

    game

--- src/app/components/organism/Header.tsx
+++ src/app/components/organism/Header.tsx
@@ -6,11 +6,11 @@ export default function Header() {
   return (
     <div className="w-full text-center">
       <div className="w-3/5 h-16 bg-slate-300 rounded-b-lg m-auto ">
-        <div className="font-bold text-3xl">
-          <Link href={"/"}>Home</Link>
-          <Button.CallSignUpModal className="bg-slate-200">Sign-Up</Button.CallSignUpModal>
-          <Button.Authentication />
-        </div>
+        {/* <div className="font-bold text-3xl"> */}
+        <Link href={"/"}>Home</Link>
+        <Button.CallSignUpModal className="bg-slate-200">Sign-Up</Button.CallSignUpModal>
+        <Button.Authentication />
+        {/* </div> */}
       </div>
     </div>
   );
